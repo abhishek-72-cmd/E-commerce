24ccccff2e9376208f3b08c8b3d1a0d39b020818
Abhishek Vibhute <abhishekvibhute2200@gmail.com>
2025-04-24 18:12:26 +0530

    Updated cors with Include any preview domains if used

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://e-commerce-qf0ecbxgk-abhisheks-projects-40d7562c.vercel.app'
+    'https://e-commerce-qf0ecbxgk-abhisheks-projects-40d7562c.vercel.app',
+     'https://e-commerce-6benpq0d9-abhisheks-projects-40d7562c.vercel.app'
   ],
   credentials: true
 }));
